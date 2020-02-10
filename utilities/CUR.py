777ec33874a279bce86e9474cdd06e65aef6a757
Rose Cersonsky <rosecers@umich.edu>
2020-02-10 14:24:26 +0100

    Doc-strings for CUR

--- utilities/CUR.py
+++ utilities/CUR.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy.sparse.linalg import svds as svd
 from .general import sorted_eig
 
-
 def approx_A(A, col_idx, row_idx=None):
+    """ Approximates the full matrix with selected features """  
 
     A_c = A[:, col_idx]
     S_c = np.linalg.pinv(A_c)
@@ -18,9 +18,8 @@ def approx_A(A, col_idx, row_idx=None):
 
     return np.matmul(A_c, np.matmul(S, A_r))
 
-
 def compute_P(A_c, S, A_r, thresh=1e-12):
-
+    """ Computes the latent-space projector for the feature matrix """
     SA = np.matmul(S, A_r)
     SA = np.matmul(SA, SA.T)
 
@@ -28,7 +27,6 @@ def compute_P(A_c, S, A_r, thresh=1e-12):
     v_SA[v_SA < thresh] = 0
 
     return np.matmul(U_SA, np.diagflat(np.sqrt(v_SA)))
-
 
 def get_Ct(X, Y, alpha=0.5, regularization=1e-6):
     """ Creates the PCovR modified covariance"""
@@ -53,8 +51,9 @@ def get_Ct(X, Y, alpha=0.5, regularization=1e-6):
 
 
 def svd_select(A, n, k=1, idxs=None, **kwargs):
-    """
-        Doc-string needed
+    """ 
+        Selection function which computes the CUR 
+        indices using the SVD Decomposition
     """
 
     if(idxs is None):
@@ -63,7 +62,7 @@ def svd_select(A, n, k=1, idxs=None, **kwargs):
     Acopy = A.copy()
 
     for nn in range(n):
-        if(nn >= len(idxs)):  # len(idxs)<=nn-1):
+        if(nn >= len(idxs)):  
             (S, v, D) = svd(Acopy, k)
             pi = (D[:k]**2.0).sum(axis=0)
             pi[idxs] = 0  # eliminate possibility of selecting same column twice
@@ -75,14 +74,14 @@ def svd_select(A, n, k=1, idxs=None, **kwargs):
 
         for i in range(Acopy.shape[1]):
             Acopy[:, i] -= v * np.dot(v, Acopy[:, i])
-        print(len(idxs), end='\r')
 
     return list(idxs)
 
 
 def pcovr_select(A, n, Y, alpha, k=1, idxs=None, **kwargs):
     """
-        Doc-string needed
+        Selection function which computes the CUR 
+        indices using the PCovR `Covariance` matrix 
     """
 
     Acopy = A.copy()
@@ -92,7 +91,7 @@ def pcovr_select(A, n, Y, alpha, k=1, idxs=None, **kwargs):
         idxs = []  # indexA is initially empty.
 
     for nn in range(n):
-        if(nn >= len(idxs)):  # len(idxs)<=nn-1):
+        if(nn >= len(idxs)):  
             try:
                 Ct = get_Ct(Acopy, Ycopy, alpha=alpha)
             except:
@@ -114,10 +113,9 @@ def pcovr_select(A, n, Y, alpha, k=1, idxs=None, **kwargs):
 
     return list(idxs)
 
-
 def update(Y_copy, X_c):
     """
-        Doc-string needed
+       Updates property matrix from selected columns 
     """
     v = np.linalg.pinv(np.matmul(X_c.T, X_c))
     v = np.matmul(X_c, v)
@@ -126,23 +124,37 @@ def update(Y_copy, X_c):
     Y_copy -= np.matmul(v, Y_copy)
 
 
+# Dictionary of Selection Functions
 selections = dict(svd=svd_select, pcovr=pcovr_select)
 
-
 class CUR:
+    """
+        Performs CUR Decomposition on a Supplied Matrix 
+
+        ---Arguments---
+        matrix: matrix to be decomposed
+        precompute: (int, tuple, Nonetype) number of columns, rows to be computed 
+                    upon instantiation. Defaults to None. 
+        feature_select: (bool) whether to compute only column indices
+        pi_function: (<func>) Importance metric and selection for the matrix
+	symmetry_tolerance: (float) Tolerance by which a matrix is symmetric
+        params: (dict) Dictionary of additional parameters to be passed to the
+                pi function
+
+        ---References---
+        1.  G.  Imbalzano,  A.  Anelli,  D.  Giofre,  S.  Klees,  J.  Behler,   
+            and M. Ceriotti, J. Chem. Phys.148, 241730 (2018) 
+    """
     def __init__(self, matrix,
                  precompute=None,
                  feature_select=False,
                  pi_function='svd',
-                 tol=1e-4,
+                 symmetry_tolerance=1e-4,
                  params={}
                  ):
-        """
-            Doc-string needed
-        """
         self.A = matrix
         self.symmetric = self.A.shape == self.A.T.shape and \
-            np.all(np.abs(self.A-self.A.T)) < tol
+            np.all(np.abs(self.A-self.A.T)) < symmetry_tolerance
         self.fs = feature_select
         if(isinstance(pi_function, str)):
             self.select = selections.get(pi_function, None)
@@ -166,9 +178,6 @@ class CUR:
                 self.idx_c, self.idx_r = self.compute_idx(*precompute)
 
     def compute_idx(self, n_c, n_r):
-        """
-            Doc-string needed
-        """
         idx_c = self.select(self.A, n_c, idxs=self.idx_c, **self.params)
         if(self.fs):
             idx_r = np.asarray(range(self.A.shape[1]))
@@ -180,7 +189,7 @@ class CUR:
 
     def compute(self, n_c, n_r=None):
         """
-            Doc-string needed
+           Compute the n_c selected columns and n_r selected rows 
         """
         if(self.fs):
             n_r = self.A.shape[1]
@@ -207,6 +216,7 @@ class CUR:
             A_r = self.A.copy()
         else:
             A_r = self.A[idx_r, :]
+
         # Compute S.
         S = np.matmul(np.matmul(np.linalg.pinv(A_c), self.A),
                       np.linalg.pinv(A_r))
@@ -214,7 +224,7 @@ class CUR:
 
     def compute_P(self, n_c):
         """
-            Doc-string needed
+           Computes the projector into latent-space for ML models 
         """
 
         A_c, S, A_r = self.compute(n_c)
@@ -222,7 +232,7 @@ class CUR:
 
     def loss(self, n_c, n_r=None):
         """
-            Doc-string needed
+            Error between approximated matrix and target 
         """
         A_c, S, A_r = self.compute(n_c, n_r)
         return np.linalg.norm(self.A - np.matmul(A_c, np.matmul(S, A_r)))/np.linalg.norm(self.A)
