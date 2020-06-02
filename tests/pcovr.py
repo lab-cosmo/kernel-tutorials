c9fc2c1949f382a04376154ce966f5d59d7993e6
Rose Cersonsky <rosecers@umich.edu>
2020-06-02 18:23:38 +0200

    Finished pcovr test

--- tests/pcovr.py
+++ tests/pcovr.py
@@ -3,12 +3,16 @@ from matplotlib import pyplot as plt
 
 from utilities.sklearn_pcovr import PCovR
 from utilities.plotting import plot_regression, plot_projection
+from utilities.colorbars import load
+load()
 
 data = np.load('./tests/CSD-test.npz')
 X = data["X"]
 Y = data["Y"]
 
-for alpha in np.linspace(0,1,3):
+fig, axes = plt.subplots(3,3, figsize=(4,4))
+
+for i, alpha in enumerate(np.linspace(0,1,3)):
     pcovr = PCovR(alpha = alpha,
                   n_components=2,
                   regularization=1e-6,
@@ -19,17 +23,28 @@ for alpha in np.linspace(0,1,3):
     Xr = pcovr.inverse_transform(T)
     Yp = pcovr.predict(X)
 
-    fig, ax = plt.subplots(1, 3, figsize=(12,4))
-    plot_projection(Y, T, fig=fig, ax=ax[0],
-                    xlabel=r"$PC_1$", ylabel=r"$PC_2$",
-                    cbar=False,
-                    title="Projection of X into Latent Space")
-    plot_regression(X, Xr, fig=fig, ax=ax[1],
-                    xlabel=r"$\mathbf{X}$", ylabel=r"$\mathbf{TP}_{TX}$",
-                    cbar=False,
-                    title="Reconstruction of X from Latent Space")
-    plot_regression(Y, Yp, fig=fig, ax=ax[2],
-                    xlabel=r"$\mathbf{Y}$", ylabel=r"$\mathbf{TP}_{TY}$",
-                    cbar=False,
-                    title="Regression of Y from Latent Space")
-    plt.show()
+    plot_projection(Y, T, fig=fig, ax=axes[0][i],
+                    x_label=r"$PC_1$", y_label=r"$PC_2$",
+                    cbar=False, s=4, font_scaled=True,
+                    cmapX='cbarHot_0.3_1.05'
+                    )
+    plot_regression(X, Xr, fig=fig, ax=axes[1][i],
+                    x_label=r"$\mathbf{X}$", y_label=r"$\mathbf{TP}_{TX}$",
+                    cbar=False, s=4, font_scaled=True,
+                    cmapY='bone_r_0.2_1.0'
+                    )
+    plot_regression(Y, Yp, fig=fig, ax=axes[2][i],
+                    x_label=r"$\mathbf{Y}$", y_label=r"$\mathbf{TP}_{TY}$",
+                    cbar=False, s=4, font_scaled=True,
+                    cmapY='bone_r_0.2_1.0'
+                    )
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+    axes[0][i].set_title(r'$\alpha =$'+f' {alpha}')
+
+for ax in axes.flatten():
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+
+plt.show()
