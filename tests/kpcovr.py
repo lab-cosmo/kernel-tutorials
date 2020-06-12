9cf822835497de10d13a5457c720a5d37f1b05e8
Rose Cersonsky <rosecersonsky@gmail.com>
2020-06-12 13:14:44 +0200

    Added precomputed kernel test

--- tests/kpcovr.py
+++ tests/kpcovr.py
@@ -3,6 +3,7 @@ from sklearn import exceptions
 from sklearn.utils.validation import check_X_y
 
 from utilities.sklearn_covr.kpcovr import KernelPCovR
+from utilities.kernels import center_kernel, gaussian_kernel
 
 tolerance = 1e-4
 
@@ -11,12 +12,12 @@ def run_tests():
     X = data["X"]
     Y = data["Y"]
 
-    # Basic Test of PCovR Errors
-    n_alpha = 21
-    alphas = np.linspace(0,1,n_alpha)
-    krr_errors = np.nan * np.zeros(n_alpha)
-    kpca_errors = np.nan * np.zeros(n_alpha)
-    for i, mixing in enumerate(alphas):
+    # Basic Test of PCovR Errors with linear kernel
+    n_mixing = 6
+    mixings = np.linspace(0,1,n_mixing)
+    krr_errors = np.nan * np.zeros(n_mixing)
+    kpca_errors = np.nan * np.zeros(n_mixing)
+    for i, mixing in enumerate(mixings):
         kpcovr = KernelPCovR(mixing = mixing, kernel='linear',
                       n_components=8,
                       fit_inverse_transform=True,
@@ -41,13 +42,64 @@ def run_tests():
     try:
         assert all(krr_errors[i]-tolerance <= krr_errors[i+1] and kpca_errors[i] >= kpca_errors[i+1]-tolerance for i in range(len(krr_errors)-1))
     except AssertionError:
-        for i, alpha in enumerate(alphas[:-1]):
+        for i, mixing in enumerate(mixings[:-1]):
             if(krr_errors[i]-tolerance > krr_errors[i+1]):
-                print(f"KRR error at α = {alpha} ({krr_errors[i]}) > α = {alphas[i+1]} {krr_errors[i+1]}")
+                print(f"KRR error at α = {mixing} ({krr_errors[i]}) > α = {mixings[i+1]} ({krr_errors[i+1]})")
             if(kpca_errors[i] < kpca_errors[i+1]-tolerance):
-                print(f"KPCA error at α = {alpha} ({kpca_errors[i]}) < α = {alphas[i+1]} {kpca_errors[i+1]}")
+                print(f"KPCA error at α = {mixing} ({kpca_errors[i]}) < α = {mixings[i+1]} ({kpca_errors[i+1]})")
 
         return False
+
+
+    # test precomputed kernel
+
+    K = gaussian_kernel(X, X, gamma=1.0)
+    n_mixing = 6
+    mixings = np.linspace(0,1,n_mixing)
+    krr_errors = np.nan * np.zeros(n_mixing)
+    kpca_errors = np.nan * np.zeros(n_mixing)
+    for i, mixing in enumerate(mixings):
+        kpcovr = KernelPCovR(mixing = mixing, kernel='precomputed',
+                      n_components=8,
+                      tol=1e-12,
+                      krr_params = dict(alpha=1,)
+                    )
+        kpcovr.fit(K, Y)
+
+        T = kpcovr.transform(K)
+        # Xr = kpcovr.inverse_transform(T)
+        Yp = kpcovr.predict(K)
+
+        krr_errors[i] = np.linalg.norm(Y-Yp)**2.0 / np.linalg.norm(Y)**2.0
+        kpca_errors[i] = np.linalg.norm(K-np.matmul(T,T.T))**2.0 / np.linalg.norm(K)**2.0
+
+        try:
+            assert not np.isnan(krr_errors[i]) and not np.isnan(kpca_errors[i])
+            print(f"Passed α = {round(mixing,4)}", end='\r')
+        except AssertionError:
+            return False
+
+    try:
+        assert all(krr_errors[i]-tolerance <= krr_errors[i+1] and kpca_errors[i] >= kpca_errors[i+1]-tolerance for i in range(len(krr_errors)-1))
+    except AssertionError:
+        for i, mixing in enumerate(mixings[:-1]):
+            if(krr_errors[i]-tolerance > krr_errors[i+1]):
+                print(f"KRR error at α = {mixing} ({krr_errors[i]}) > α = {mixings[i+1]} ({krr_errors[i+1]})")
+            if(kpca_errors[i] < kpca_errors[i+1]-tolerance):
+                print(f"KPCA error at α = {mixing} ({kpca_errors[i]}) < α = {mixings[i+1]} ({kpca_errors[i+1]})")
+
+        return False
+
+    # test that inverse transform does not work with precomputed kernel
+    try:
+        kpcovr = KernelPCovR(mixing = mixing, kernel='precomputed',
+                      n_components=8,
+                      tol=1e-12,
+                      fit_inverse_transform = True,
+                      krr_params = dict(mixing=1e-2,)
+                    )
+    except ValueError:
+        pass
 
     return True
 if __name__ == "__main__":
