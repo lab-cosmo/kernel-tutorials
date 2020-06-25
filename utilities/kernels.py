53bc882a25ac4e3ae77dbb4d5f41180c0e194ab9
bhelfrecht <ben.helfrecht@gmail.com>
2020-06-25 13:27:23 +0200

    Change structure of kernel functions for consistent behavior between Gaussian and linear kernels for structure-based analysis

--- utilities/kernels.py
+++ utilities/kernels.py
@@ -1,68 +1,110 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 
+def self_linear_kernel(XA):
+    """
+        Build a dot product kernel between all samples in XA
 
-def linear_kernel(XA, XB):
+        ---Arguments---
+        XA: vectors of data from which to build the kernel,
+            where each row is a sample and each column is a feature.
+            If XA is a list, the kernel is averaged over the list
+
+        ---Returns---
+        K: dot product kernel matrix between XA samples
+    """
+
+    K = np.zeros((len(XA), len(XA)))
+
+    flag_A = isinstance(XA, list)
+
+    # XA structures
+    if flag_A:
+        for idx_i in range(len(XA)):
+            for idx_j in range(idx_i, len(XA)):
+                kij = np.matmul(XA[idx_i], XA[idx_j].T)
+                K[idx_i, idx_j] = K[idx_j, idx_i] = kij.mean()
+
+    # XA environments
+    else:
+        K = np.matmul(XA, XA.T)
+
+    return K
+
+def linear_kernel(XA, XB=None):
     """
         Builds a dot product kernel
 
         ---Arguments---
         XA, XB: vectors of data from which to build the kernel,
-            where each row is a sample and each column is a feature
+            where each row is a sample and each column is a feature.
+            If XA or XB is a list, the kernel is averaged over the list
 
         ---Returns---
-        K: dot product kernel between XA and XB
+        K: dot product kernel between XA (and XB)
     """
 
-    K = np.matmul(XA, XB.T)
+    flag_A = isinstance(XA, list)
+    flag_B = isinstance(XB, list)
 
-    return K
+    if XB is None:
+        return self_linear_kernel(XA)
 
-
-def summed_kernel(XA, XB, kernel_func, **kwargs):
-    """
-        Provides the kernel for for properties which are learned on the sum of
-        soap vectors. Necessary for non-linear kernels.
-    """
-    K = np.zeros((XA.shape[0], XB.shape[0]))
-
-    for idx_i in range(XA.shape[0]):
-        for idx_j in range(XB.shape[0]):
-            kij = kernel_func(XA[idx_i], XB[idx_j], **kwargs)
-            K[idx_i, idx_j] = kij.sum()
-
-    return K
-
+<<<<<<< HEAD
 def self_gaussian_kernel(XA, fps_compute=None, gamma=1.0):
         """
         Build a Gaussian kernel between all samples in XA
         """
         K = np.zeros((len(XA), len(XA)))
+=======
+    else:
+        K = np.zeros((len(XA), len(XB)))
+>>>>>>> 173f28b... Change structure of kernel functions for consistent behavior between Gaussian and linear kernels for structure-based analysis
 
-        flag_A = len(XA.shape)==1
+        # XA and XB structures
+        if flag_A and flag_B:
+            for idx_i in range(len(XA)):
+                for idx_j in (range(len(XB))):
+                    ki = np.matmul(XA[idx_i], XB[idx_i].T)
+                    K[idx_i, idx_j] = ki.mean()
 
-        # XA structures
-        if flag_A:
+        # XA structures, XB environments
+        elif flag_A:
             for idx_i in range(len(XA)):
                 for idx_j in range(idx_i, len(XA)):
                     kij = np.exp(-gamma*cdist(XA[idx_i][:, fps_compute], XA[idx_j][:, fps_compute], metric="sqeuclidean"))
                     K[idx_i, idx_j] = K[idx_j, idx_i] = kij.mean()
 
-        # XA environments
+        # XA environments, XB structures
+        elif flag_B:
+            for idx_j in range(len(XB)):
+                kj = np.matmul(XA, XB[idx_j].T)
+                K[:, idx_j] = ki.mean(axis=1)
+
+        # XA and XB environments
         else:
-            K = np.exp(-gamma*cdist(XA, XA, metric="sqeuclidean"))
+            K = np.matmul(XA, XB.T)
 
 {value for value in variable}        return K
 
 def gaussian_kernel(XA, XB=None, fps_compute=None, gamma=1.0):
     """
-    Build a Gaussian kernel between all samples in XA,
-    or between XA and XB (if provided)
-    """
-    flag_A = len(XA.shape)==1
-    flag_B = XB is None or len(XB.shape)==1
+    Build a Gaussian kernel between all samples in XA and XB
 
-    if(XB is None):
+    ---Arguments---
+    XA, XB: vectors of data from which to build the kernel,
+        where each row is a sample and each column is a feature.
+        If XA or XB is a list, the kernel is averaged over the list
+    gamma: kernel width
+
+    ---Returns---
+    K: gaussian kernel matrix between XA (and XB)
+    """
+
+    flag_A = isinstance(XA, list)
+    flag_B = isinstance(XB, list)
+
+    if XB is None:
         return self_gaussian_kernel(XA, gamma=gamma)
 
     else:
