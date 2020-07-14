e61b4f4f47a78bc2807fd53f196487a9605e58f0
Michele Ceriotti <michele.ceriotti@gmail.com>
2020-07-14 14:17:18 +0200

    Kernel centering and scaling, first commit

--- utilities/scalers.py
+++ utilities/scalers.py
@@ -50,3 +50,91 @@ class NormalizeScaler(TransformerMixin, BaseEstimator):
         if self.n_samples_seen_ == 0 :
             raise sk.exceptions.NotFittedError("This "+type(self).__name__+" instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
         return self.scale_*(X-self.mean_)
+
+class KernelNormalizer(TransformerMixin, BaseEstimator):
+    """ A more flexible kernel centerer that can work for both full and sparse 
+    kernel frameworks, and that can normalize, center, or fully standardize the kernel. """
+    
+    def __init__(self, *, with_mean=True, with_norm=True, rcond=None):
+        """ Initialize KernelNormalizer. Defines whether it will subtract mean
+        (with_mean=True), apply normalization (with_norm=True) and whether it will
+        normalize each feature separately (per_feature=True). """
+        
+        self.__with_mean = with_mean
+        self.__with_norm = with_norm
+        self.__rcond = rcond
+        self.__sparse_kernel = False
+        self.n_samples_seen_ = 0
+        self.n_active_ = 0
+
+    def fit(self, K, y=None, K_active=None):
+        """ Compute mean and scaling to be applied to kernels. If K_active is given,
+        then it works under the assumption we are dealing with sparse, Nystroem approximation
+        kernels. K is then the n_sampex x n_active train set matrix. 
+        Otherwise, K should be the n_train x n_train full train set matrix. """        
+        
+        if K_active is not None:
+            self.__sparse_kernel = True
+            self.n_active_ = K_active.shape[0]
+            if K_active.shape[0] != K_active.shape[1]:
+                raise ValueError("K_active should be the square, active set kernel. ")
+                
+        self.n_samples_seen_ = K.shape[0]
+        if self.__sparse_kernel:
+            if K_shape[1] != self.n_active_:
+                raise ValueError("For sparse kernels, K should be train-active set kernel. ")
+        elif K.shape[0] != K.shape[1]:
+            raise ValueError("K should be the square, train set kernel. ")
+        
+        self.K_train_mean_ = 0
+        
+        K_diag = K.diagonal().copy()
+        if self.__with_mean:
+            self.K_train_row_mean_ = K.mean(axis=0)
+            self.K_train_mean_ = K.mean()
+            if not self.__sparse_kernel:
+                K_diag +=  - self.K_train_row_mean_*2 + self.K_train_mean_        
+                    
+        self.scale_ = 1.0
+        if self.__with_norm:
+            if self.__sparse_kernel:
+                var = np.sqrt( 
+                        np.trace(K@np.linalg.lstsq(K_active, K.T, self.__rcond)[0])/
+                        self.n_samples_seen_
+                        )
+            else:
+                var = K_diag.mean()            
+            self.scale_ = 1.0/var 
+                
+        return self
+
+    def transform(self, K_test, y=None, K_test_train = None):
+        """ Normalize a test kernel based on previously computed mean and scaling. 
+        It can also normalize an arbitrary kernel matrix provided one gives the kernels 
+        between rows and columns and the train set, with K_test_train = (K_test_rows_train, K_test_cols_train).
+        """
+        
+        if self.n_samples_seen_ == 0 :
+            raise sk.exceptions.NotFittedError("This "+type(self).__name__+" instance is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
+        
+        K_std = K_test.copy()
+        if self.__sparse_kernel:
+            if K_test.shape[1] != self.n_active_:
+                raise ValueError("Test kernel must have n_active as second dimension")
+            if self.__with_mean:
+                K_std -= self.K_train_row_mean_
+        else:
+            if K_test_train is None:
+                if K_test.shape[1] != self.n_samples_seen_:
+                    raise ValueError("Test kernel must have n_train as second dimension")
+                if self.__with_mean:
+                    print(self.K_train_row_mean_)
+                    print(K_test.mean(axis=0))
+                    K_std += self.K_train_mean_ - np.add.outer(K_test.mean(axis=1), self.K_train_row_mean_)          
+            else:
+                K_rows, K_cols = K_test_train
+                K_std += self.K_train_mean_ - np.add.outer( K_rows.mean(axis=0), K_cols.mean(axis=0) )
+        if self.__with_norm:
+            K_std *= self.scale_
+            
+        return K_std
